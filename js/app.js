e2a97a08481ad8c83313c2a8596f20ef14d589eb
umlaufisadora <umlaufisadora@gmail.com>
2026-07-27 10:38:20 -0300

    feat: replace and prepare environment to use the API

---
+++ js/app.js
@@ -0,0 +1,23 @@
+// const describe = document.getElementById('btn-describe');
+// let count = 0;
+
+//     describe.addEventListener('click', () =>
+//     {
+//         if(count <1)
+//         {
+//             count +=1;
+//             const list = document.createElement('li');
+//             const lista = document.getElementById('listButton')
+
+
+//             list.classList.add('task');
+
+//             const content = `<input placeholder="Digite o nome da cidade..." id="input-list"></input>`;
+
+//             list.innerHTML = content;
+//             lista.appendChild(list)
+//         }
+//          console.log(count)
+//     });
+
+//API KEY: 9bfc94f07adf1576638ea4934a72a9f3
